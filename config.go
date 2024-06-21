a3f5e68d031585eafc32d558a8e79ebcb679304e
yewei <3167496206@qq.com>
2024-06-21 18:29:14 +0800

    数据库和redis的配置文件

--- config.go
+++ config.go
@@ -2,33 +2,24 @@ package config
 
 import (
 	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"xorm.io/xorm"
 	"github.com/gomodule/redigo/redis"
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql" 
 )
 
-var RedisPool *redis.Pool
-var db *sql.DB //连接池对象
-func initDB() (err error) {
-	//数据库
-	//用户名:密码啊@tcp(ip:端口)/数据库的名字
-	dsn := "root:123@tcp(127.0.0.1:3306)/test"
-	//连接数据集
-	db, err = sql.Open("mysql", dsn) 
-	if err != nil {
-		return
-	}
-	err = db.Ping() //尝试连接数据库
-	if err != nil {
-		return
-	}
-	fmt.Println("连接数据库成功~")
-	//设置数据库连接池的最大连接数
-	db.SetMaxIdleConns(10)
-	return
+var (
+	DBEngine *xorm.Engine
+	RedisPool *redis.Pool
+)
 
+func InitDB() {
+	var err error
+	DBEngine, err = xorm.NewEngine("mysql", "user:password@/dbname?charset=utf8")
+	if err != nil {
+		fmt.Println("Database connection failed:", err)
+		return
+	}
 }
-
 
 func InitRedis() {
 	RedisPool = &redis.Pool{
